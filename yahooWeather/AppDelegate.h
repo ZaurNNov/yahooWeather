72a93e324c70e709522503f60c28332404813ff3
Zaur Giyasov <zagiyas@mera.ru>
2018-06-13 18:26:07 +0300

    Initial Commit

---
+++ yahooWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yahooWeather
+//
+//  Created by Zaur Giyasov on 13/06/2018.
+//  Copyright © 2018 Zaur Giyasov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ yahooWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  yahooWeather
+//
+//  Created by Zaur Giyasov on 13/06/2018.
+//  Copyright © 2018 Zaur Giyasov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
